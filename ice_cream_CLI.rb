61a6e8ce211a4c9b5862bdac4fd2859dade35384
firyalmahakhan <firyalmahak@gmail.com>
2018-08-09 18:11:17 +0000

    automatically backed up by learn

---
+++ ice_cream_CLI.rb
@@ -0,0 +1,19 @@
+def ice_cream 
+  puts "What is your favorite type of ice cream?"
+  ice cream = gets.chomp.downcase 
+  
+  if ice_cream == "coffee" 
+    puts "Don't eat too close to bed time!"
+    elsif ice_cream == "birthday cake"
+    puts "Happy Birthday to you!"
+    elsif ice_cream == "cookie dough"
+    puts "Yum I love cookie dough too!"
+    elsif "Mint Chocolate Chip"
+  puts "You're cooler than mint!"
+else 
+  puts "Hmm, I don't know that flavor!"
+  
+end 
+
+  
+end
