58f396e0e5f8a11a51b4c5aded8904a74469b291
firyalmahakhan <firyalmahak@gmail.com>
2018-08-09 18:57:20 +0000

    automatically backed up by learn

--- calc.rb
+++ calc.rb
@@ -1,3 +1,16 @@
+Order = {
+"appetizers" => ["soup", "salad", "olive garden" "fruit platter"],
+"main course" => ["steak", "chicken", "lasagna", "spaghetti"],
+"dessert" => ["Cake", "Brownie", "Ice Cream" "Donut"]
+}
+def Order
+  puts "What #{appetizer} did you order?" 
+
+
+
+
+
+
 def tip_calculator 
   puts "What percent would you like to tip?"
   tip = gets.chomp.to_f
