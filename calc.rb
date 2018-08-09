f7e1d035078e66dc354ad839d98a8393af039f3f
firyalmahakhan <firyalmahak@gmail.com>
2018-08-09 18:37:31 +0000

    automatically backed up by learn

--- calc.rb
+++ calc.rb
@@ -2,9 +2,11 @@ def tip_calculator
   puts "What percent would you like to tip?"
   tip = gets.chomp
   
+  puts "What  is your total?" 
+  total = gets.chomp
   
   total = gets.chomp
   total = (tip/100) + 1 
   tip_percentage * total = total_with_tip
-  
-  
+end
+ tip_calculator
